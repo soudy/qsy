1854eed6739f5c3771cc1f45846a798ccb10835b
Steven Oud <soud@protonmail.com>
2019-05-02 13:59:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='qsy',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     author='Steven Oud',
     author_email='soud@pm.me',
-    description='A quantum computer state vector simulator',
+    description='A quantum computer state vector simulator and quantum assembly language',
     license='MIT',
     install_requires=['numpy>=1.13', 'ply>=3'],
     entry_points = {
