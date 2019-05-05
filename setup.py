5520da521051ce1f0e63bc60aba86e6ec5da7887
Steven Oud <soud@protonmail.com>
2019-05-05 12:52:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ import re
 with open('./qsy/__init__.py') as f:
     version, = re.findall('__version__ = \'(.*)\'', f.read())
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='qsy',
     version=version,
@@ -12,9 +15,21 @@ setup(
     author='Steven Oud',
     author_email='soud@pm.me',
     description='A quantum computer state vector/stabilizer circuit simulator and assembly language',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/soudy/qsy',
     license='MIT',
     install_requires=['numpy>=1.13', 'ply>=3'],
-    entry_points = {
+    keywords=['quantum', 'computing', 'simulator', 'stabilizer', 'circuit',
+              'assembly', 'chp', 'statevector']
+    entry_points={
         'console_scripts': ['qsyasm=qsyasm.cli:main']
-    }
+    },
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Assemblers'
+    ]
 )
