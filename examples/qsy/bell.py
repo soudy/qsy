234df99040221d335dd93c93b69cc8c09c85ed67
Steven Oud <soud@protonmail.com>
2019-06-25 12:02:50 +0200

    Update Python example

--- examples/qsy/bell.py
+++ examples/qsy/bell.py
@@ -2,6 +2,6 @@ from qsy import QuantumRegister, gates
 
 qr = QuantumRegister(2)
 qr.apply_gate(gates.H, 0)
-qr.apply_gate(gates.C(gates.X), 0, 1)
+qr.apply_gate(gates.CX, 0, 1)
 
 print(qr.to_dirac())
